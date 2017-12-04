0a30853fca3fb34ddb9b2d1bb65026cf3445abc7
mosabalturk <mosabalturk@gmail.com>
2017-12-04 14:44:41 +0300

    after adding list of list result first time3333

---
+++ Source/Analyzer/cppFile.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Analyzer
+{
+    public class cppFile
+    {
+        public code code { set; get; }
+        public List<List<tokenCounter>> KwOpDtTCnts { get { return code.KwOpDtTC(code); } }
+        List<token> holeCodeTokens;//done
+        private string cStr;//done
+        private string fname;//done
+        private List<token> defines = new List<token>();
+        //# define varName value
+        // define.type = value of define 
+        // defin.lexeme = varName
+        private List<string> libraries = new List<string>();//done
+        public List<string> getLibraries { get { return libraries; } }
+        public List<token> getDefines { get { return defines; } }//ساويه من نوع ايدنتيفير
+        
+        public cppFile(string codestr, string filename)
+        {
+            codestr = Program.RemoveCommentsAndSpaces(codestr);//delete comments
+
+            this.cStr = codestr;//add hole code to cStr string
+
+            this.fname = filename;//filename or class or struct name
+
+            Analyzer temp = new Analyzer(); // anlyzer class is the class that analyze code to tokens and lexemes
+
+            holeCodeTokens = temp.Result2(codestr);//analyze code to tokens and lexemes to this list
+
+            findLibrariesAndDefines();
+
+            pointersArraysAnalzer();
+            //remove *s pointers and make pointer true to the identifier that defined as pointer
+            // remove [ , ] and the value between them from arrays and add them to the propreties of identifier that defined as an array
+            //globalScobeTokens = new List<token>(allCodeTokens); // copy allCodeTokens list
+            //findClassesAndStructs();
+            //Analyzer.structAsDatatype(allCodeTokens, globalScobeTokens, structes.Select(a => a.filename).ToList(), structes.Select(a => a.typedefName).ToList());
+            //findMain();
+            //findFunctionsAndPrototypes();
+            // count();
+            //findVar();
+            code = new code(codestr, filename, holeCodeTokens, ref holeCodeTokens, new List<identifier>(), new List<pointer>(), new List<array>());
+            code.spitYourVariablesLn(code);
+        }
+        
+        public void findLibrariesAndDefines()
+        {
+            if (holeCodeTokens != null)
+                for (int i = 0; i < holeCodeTokens.Count; i++)
+                {
+                    if ((holeCodeTokens[i].getLexeme() == "#") && (holeCodeTokens[i + 1].getLexeme() == "include"))
+                    {
+                        string lib = "#include";
+                        int cntr = 1;
+                        while ((holeCodeTokens[i + cntr].getLexeme() != ">") && (holeCodeTokens[i + cntr].getType() != "vstring"))
+                        { lib += holeCodeTokens[i + cntr + 1].getLexeme(); cntr++; }
+                        cntr++;
+                        for (int j = i; j < i + cntr - 1; j++)
+                            holeCodeTokens.RemoveAt(i + 1);
+                        holeCodeTokens[i].setLexeme(lib);
+                        holeCodeTokens[i].setType("library");
+                        libraries.Add(lib);
+                    }
+                    if ((holeCodeTokens[i].getLexeme() == "#") && (holeCodeTokens[i + 1].getLexeme() == "define"))
+                    {
+                        holeCodeTokens[i + 2].setType(holeCodeTokens[i + 3].getLexeme());
+                        defines.Add(holeCodeTokens[i + 2]);
+                        holeCodeTokens.RemoveAt(i);
+                        holeCodeTokens.RemoveAt(i);
+                        holeCodeTokens.RemoveAt(i);//error it could be operation like (2/2)
+                        holeCodeTokens.RemoveAt(i);
+                        i--;
+                    }
+                }
+        }
+        public void pointersArraysAnalzer()
+        {
+            List<token> remove = new List<token>();
+            for (int i = 0; i < holeCodeTokens.Count - 1; i++)
+            {
+
+                bool structPtr = i > 1 ? ((holeCodeTokens[i - 1].getLexeme() == "struct") && (holeCodeTokens[i].isIdentifierTokenObject()) && (holeCodeTokens[i + 1].getLexeme() == "*")) : (false);
+                if ((holeCodeTokens[i].isDatatype() || (structPtr)) && holeCodeTokens[i + 1].getLexeme() == "*")
+                {
+                    int k = 0;//  * counter
+                    int j = i; //  i is data type index
+                    while (holeCodeTokens[++j].getLexeme() == "*")// j is  * index
+                        k++;
+                    // now j is identifier index
+                    if (holeCodeTokens[j].isIdentifierTokenObject())
+                    {// modifiye porpreties of identifier
+                        pointer p = new pointer(holeCodeTokens[j], k);
+                        p.dataType = holeCodeTokens[i];
+                        holeCodeTokens[j] = p;
+                    }
+                    i++;
+                    int howManyPointers = j - i;
+                    while (howManyPointers > 0)
+                    {
+                        token t = holeCodeTokens[i];
+                        //remove all *s from result list
+                        holeCodeTokens.Remove(t);
+                        howManyPointers--;
+                    }
+                    i--;
+                    continue;
+                }
+
+                bool IsarrayInit = i < holeCodeTokens.Count - 1 ? holeCodeTokens[i].getType() == "identifier" && holeCodeTokens[i + 1].getLexeme() == "[" : false;
+                if (IsarrayInit)
+                {
+                    int j = i + 1;
+                    array newArray = new array(holeCodeTokens[i]);
+                    List<int> arr = new List<int>();
+                    if ((i > 0) && (holeCodeTokens[i - 1].isDatatype()))
+                        newArray.dataType = holeCodeTokens[i - 1];
+                    while (holeCodeTokens[j].getLexeme() == "[")
+                    {
+                        bool arrayWithoutBoundaies = (holeCodeTokens[j].getLexeme() == "[") && (holeCodeTokens[j + 1].getLexeme() == "]");
+                        if (arrayWithoutBoundaies)
+                        {
+
+                            int valu = holeCodeTokens[j + 3].getLexeme().Length - 2;
+                            remove.Add(holeCodeTokens[j]);
+                            remove.Add(holeCodeTokens[j + 1]);
+                            newArray.arrayIndices.Add(valu);
+                            j += 2;
+                        }
+                        else if ((holeCodeTokens[j].getLexeme() == "[") && (holeCodeTokens[j + 2].getLexeme() == "]"))
+                        {
+
+                            int valu = 0;
+                            if (holeCodeTokens[j + 1].isValue())
+                                valu = Convert.ToInt32(holeCodeTokens[j + 1].getLexeme());
+
+                            if (holeCodeTokens[j + 1].isIdentifierTokenObject())
+                                foreach (token t in defines)
+                                    if (t.getLexeme() == holeCodeTokens[j + 1].getLexeme())
+                                        valu = Convert.ToInt32(t.getType());
+                            newArray.arrayIndices.Add(valu);
+                            remove.Add(holeCodeTokens[j]);
+                            remove.Add(holeCodeTokens[j + 1]);
+                            remove.Add(holeCodeTokens[j + 2]);
+                            j += 3;
+                        }
+                        else
+                        {
+                            int k = j + 1;
+                            remove.Add(holeCodeTokens[j]);
+                            int paran = 0;
+                            while ((holeCodeTokens[k].getLexeme() != "]") || (paran != 0))
+                            {
+                                if (holeCodeTokens[k].getLexeme() == "[")
+                                {
+                                    paran++;
+                                }
+                                if (holeCodeTokens[k].getLexeme() == "]")
+                                {
+                                    paran--;
+                                }
+                                remove.Add(holeCodeTokens[k++]);
+                            }
+                            j = k;
+                            remove.Add(holeCodeTokens[j]);
+                            newArray.arrayIndices.Add(0);
+                            j = ++k;
+                        }
+
+                    }
+                    holeCodeTokens[i] = newArray;
+                    i = j;
+                    continue;
+                }
+
+            }
+            foreach (token t in remove)
+            {
+                ///System.Windows.Forms.MessageBox.Show(t.id.ToString());
+                holeCodeTokens.Remove(t);
+
+            }
+            //defect of this function
+            // when deleting [] the code coulde be like this :
+            //	   setup_duration[values[0]][values[1]]=values[2];
+            //     setup_duration[values[1]][values[0]] = values[2];
+            // and the result will be 
+            // setup_duration = values 
+            // setup_duration = values
+        }
+
+    }
+
+}
---
+++ Source/Analyzer/function.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Analyzer
+{
+    public class function
+    {
+
+        private List<variableCounter> variablesCounter = new List<variableCounter>();
+        private List<pointerCounter> pointersCounter = new List<pointerCounter>();
+        private List<arrayCounter> arraysCounter = new List<arrayCounter>();
+        private List<tokenCounter> KeyWordsCounter = new List<tokenCounter>();
+        private List<tokenCounter> dataTypesCounter = new List<tokenCounter>();
+        private List<tokenCounter> valuesCounter = new List<tokenCounter>();
+        private List<tokenCounter> operationsCounter = new List<tokenCounter>();
+
+        bool protoType = false;
+        public bool recursive { get; set; }
+        private string fname;
+        private string funcBody;
+        public string funcDataType { get; set; }
+        List<token> tokens;
+        List<identifier> thisScopeVars = new List<identifier>();
+        List<identifier> upperLevelVar = new List<identifier>();
+        List<pointer> upperLevelPointers = new List<pointer>();
+        List<pointer> thisScopePointers = new List<pointer>();
+        List<array> upperLevelArray = new List<array>();
+        List<array> thisScopeArray = new List<array>();
+        List<token> holeCodeTokens;
+        public void setTokens(List<token> funcTokens) { tokens = funcTokens; }
+        private List<identifier> parameters = new List<identifier>();
+        public string funcAsStr { get { return funcBody; } set { funcBody = value; } }
+        public List<identifier> funcParameters { get { return parameters; } }
+        public function(string code, ref List<token> tokens, List<identifier> parameters, string datatype, string funcName, ref List<token> holeCodeTokens, List<identifier> upperLevelVar, List<pointer> upperLevelPointers, List<array> upperLevelArray)
+        {
+            this.holeCodeTokens = holeCodeTokens;
+            this.upperLevelVar = upperLevelVar;
+            this.upperLevelPointers = upperLevelPointers;
+            this.upperLevelArray = upperLevelArray;
+            this.funcBody = code;
+            this.tokens = tokens;
+            this.parameters = parameters;
+            this.funcDataType = datatype;
+            this.fname = funcName;
+            for (int j = 0; j < tokens.Count(); j++)
+                if ((tokens[j].getLexeme() == "return") && (tokens[j + 1].getLexeme() == funcName))
+                    this.recursive = true;
+            //findVar();
+            count();
+        }
+        public function(List<identifier> parameters, string datatype, string funcName, bool protoType = true)
+        {
+            this.protoType = protoType;
+            this.parameters = parameters;
+            this.funcDataType = datatype;
+            this.fname = funcName;
+        }
+        public string funcname { get { return fname; } set { fname = value; } }
+        public void count()
+        {
+            foreach (token t in tokens)
+            {
+                if (t.isOperation())
+                    tokenCounter.AddOneByLexeme(t, operationsCounter);
+                else if (t.isKeyword())
+                { tokenCounter.AddOneByLexeme(t, KeyWordsCounter); }
+                else if (t.isDatatype())
+                    tokenCounter.AddOneByLexeme(t, dataTypesCounter);
+                else if (t.isValue())
+                    tokenCounter.AddOneValueByDataType(t, valuesCounter);
+                //else if (t.isIdentifier())
+                //    token.addOne(t, variablesCounterGS, true);
+                //else if (t.isPointer)
+                //    token.addOne(t, pointersCounterGS, true);
+                //else if (t.isArray)
+                //    token.addOne(t, arraysCounterGS, true);
+            }
+        }
+
+        public void findVar()
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                identifier id;
+                if ((tokens[i].getType() == "identifier") && !tokens[i].isPointer && !tokens[i].isArray)
+                {
+                    if ((i > 0) && tokens[i].isIdentifierTokenObject() && (tokens[i - 1].isDatatype()))
+                    {
+                        id = new identifier(tokens[i]);
+                        if ((i > 0) && (tokens[i - 1].isDatatype()))
+                            id.dataType = tokens[i - 1];
+                        thisScopeVars.Add(id);
+                        for (int q = 0; q < holeCodeTokens.Count; q++)
+                            if (holeCodeTokens[q].id == tokens[i].id)
+                                holeCodeTokens[q] = id;
+                        tokens[i] = id;
+                    }
+                    else if (thisScopeVars.Select(a => a.getLexeme()).ToList().Contains(tokens[i].getLexeme()))
+                    {
+                        id = thisScopeVars.Find(a => a.getLexeme() == tokens[i].getLexeme());
+
+                        for (int q = 0; q < holeCodeTokens.Count; q++)
+                            if (holeCodeTokens[q].id == tokens[i].id)
+                                holeCodeTokens[q] = id;
+
+                        tokens[i] = id;
+                    }
+                    else if (upperLevelVar.Select(a => a.getLexeme()).ToList().Contains(tokens[i].getLexeme()))
+                    {
+                        id = upperLevelVar.Find(a => a.getLexeme() == tokens[i].getLexeme());
+                        for (int q = 0; q < holeCodeTokens.Count; q++)
+                            if (holeCodeTokens[q].id == tokens[i].id)
+                                holeCodeTokens[q] = id;
+                        tokens[i] = id;
+
+                    }
+
+                    else
+                    {
+                        id = new identifier(tokens[i]);
+                        if ((i > 0) && (tokens[i - 1].isDatatype()))
+                            id.dataType = tokens[i - 1];
+                        thisScopeVars.Add(id);
+                        for (int q = 0; q < holeCodeTokens.Count; q++)
+                            if (holeCodeTokens[q].id == tokens[i].id)
+                                holeCodeTokens[q] = id;
+                        tokens[i] = id;
+
+                    }
+                }
+
+            }
+        }
+    }
+}
